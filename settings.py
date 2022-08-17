b993f87ce8951bc6a56fe6af576abc305716210c
yora3vs3 <mayora.brian@yahoo.com>
2022-08-17 07:28:25 +0000

    Aug 17, 2022, 10:28 AM

--- settings.py
+++ settings.py
@@ -14,19 +14,19 @@ class Settings():
 
         # Bullet settings
         self.bullet_speed_factor = 3
-        self.bullet_width = 10
-        self.bullet_height = 15
-        self.bullet_color = 60, 60, 60
-        self.bullets_allowed = 3
+        self.bullet_width = 1000
+        self.bullet_height = 1
+        self.bullet_color = 50, 60, 30
+        self.bullets_allowed = 10
 
         # Alien settings
-        self.alien_speed_factor = 1
+        self.alien_speed_factor = 5
         
         # Creating settings for fleet direction
         self.fleet_drop_speed = 10
         
         # Fleet direction of 1 rep right; -1 rep left
-        self.fleet_direction = 1
+        self.fleet_direction = 2
 
         # How quick;y the game speeds up
         self.speedup_scale = 1.1
